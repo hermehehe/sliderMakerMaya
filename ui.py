8f9c62e665ee16cd8195ae88e6faa7301c33d4d5
ENVY_HERMELA\herme <hermegebr21@gmail.com>
2024-12-04 16:13:28 -0500

    added check boxes ui, no functionality to boxes yet

--- ui.py
+++ ui.py
@@ -29,10 +29,17 @@ def show_ui():
     cmds.rowLayout(numberOfColumns=2, adjustableColumn=[1, 2])
     cmds.button(label='Set Min', height=30, ann='Save slider start key after moving controls', command=set_min_key)
     cmds.button(label='Set Max', height=30, ann='Save slider end key after moving controls', command=set_max_key)
+    # check boxes
+    cmds.setParent('..')
+    cmds.columnLayout(adjustableColumn=True, rowSpacing=5)
+    cmds.rowLayout(numberOfColumns=2, adjustableColumn=[1, 2])
+    cmds.checkBox(label='Mirror', ann='Will apply keys to controls opposite from selected as well')
+    cmds.checkBox(label='Zero as Default', ann='Sets 0 as midpoint for slider controller')
 
     # apply keys
     cmds.setParent('..')
     cmds.columnLayout(adjustableColumn=True, rowSpacing=5)
+    cmds.text(label='Select one or multiple rigs then apply slider', font="boldLabelFont", height=30)
     cmds.button(label='Apply', height=60, ann='Create slider and set driven keys onto selected controls',
                 bgc=[0.55, 0.19, 0.60], command=apply_attr_dict)
 
@@ -77,8 +84,7 @@ def get_attr_dict():
     name = get_name()
     if name is None:
         return
-    slider_name = '{}_slider'.format(name)
-    file_name = '{}.{}'.format(slider_name, core.EXT)
+    file_name = '{}_slider.{}'.format(name, core.EXT)
     file_path = core.os.path.join(core.ROOT_DIR, file_name)
 
     if not core.os.path.exists(file_path):
@@ -117,16 +123,38 @@ def set_max_key(*args):
 
 
 def apply_attr_dict(*args):
-    """ Creates slider controller and sets driven keys from attribute dictionary"""
-    name = get_name()
-    if name is None:
+    """ Creates slider controller NURBS polygon group and sets all driven keys from attribute dictionary"""
+    slider_name = get_name()
+    if slider_name is None:
         return
 
     attr_dict = get_attr_dict()
     if attr_dict is None:
         return
-    selected = list(attr_dict.keys())[0]
-    controls = core.create_slider_ctrl(name, selected)
-    limit_dict = core.create_slider_attr(controls)
-    core.set_driven_keys(attr_dict, limit_dict, controls)
 
+    selected_namespace_list = core.get_selected_namespace()
+    if len(selected_namespace_list) == 0:
+        cmds.warning("Please select at least one rig!")
+        return
+
+    for namespace in selected_namespace_list:
+        # rename attributes in dictionary to match selected namespace
+        renamed_attr_dict = {}
+        renamed_attr_dict = core.rename_attr_dict(namespace, attr_dict)
+        if not renamed_attr_dict:
+            cmds.warning('Cannot apply {} slider to {}'.format(slider_name, namespace))
+            continue
+        # create NURBS polygons slider
+        controls = core.create_slider_ctrl(slider_name, namespace)
+        # move slider to rig location
+        selected = list(renamed_attr_dict.keys())[0]
+        core.slider_position(controls, selected)
+        # set driven keys
+        limit_dict = core.create_slider_attr(controls)
+        core.set_driven_keys(renamed_attr_dict, limit_dict, controls)
+
+    # cleanup removing attr dict json file
+    file_name = '{}_slider.{}'.format(slider_name, core.EXT)
+    file_path = core.os.path.join(core.ROOT_DIR, file_name)
+
+    core.remove_json(file_path)
